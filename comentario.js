167e205aedc0ee2c76d9ce0259ae7bb0fc2aa68d
Pablo Henrique <72884618+PabloXI@users.noreply.github.com>
2022-01-18 20:08:46 -0300

    Add files via upload

---
+++ comentario.js
@@ -0,0 +1,11 @@
+//console.log é usado para exibir algo na tela do computador!.
+console.log("Bom Dia!");
+console.log("Boa Tarde!");
+//console.log("Boa Noite!");
+
+/* 
+     *Esse é um comentario
+    * de multiplas
+   * linhas
+*/
+// # Tambem serve para fazer comentarios em algumas linguagens (Phyton)
---
+++ senteca_2.js
@@ -0,0 +1,7 @@
+console.log(1 + 3); // Aparece o resultado da subtração.'4'
+// Quando quiser dizitar um Texto pode se usar aspas duplas" ", ou aspas simples ''(javascript)
+console.log('Texto bem legal');
+// A ordem do texto e bem importante exemplo:
+console.log('Passo #01')
+console.log('Passo #02')
+console.log('Passo #03')
