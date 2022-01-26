a9b91fe8d2761fa17f951388426fbb68228a6cd9
Pablo Henrique <72884618+PabloXI@users.noreply.github.com>
2022-01-26 19:29:41 -0300

    Add files via upload

---
+++ dados_3.js
@@ -0,0 +1,12 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade); // typeof informa qual e o tipo. "number"
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = true // ou false
+console.log(typeof estaChovendo); // valor boolean ou seja verdadeiro ou falso
+
+console.log(typeof "Teste"); // string
+console.log(typeof 'Teste');
